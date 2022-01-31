3e53490910c9a131929083e0e2d74da8a66cdb5b
gochinj <jeff@gochin.com>
2022-01-31 17:59:54 -0500

    Convert unit tests to typescript.

--- test/Token.ts
+++ test/Token.ts
@@ -1,13 +1,18 @@
-const { ethers } = require("hardhat");
-const { expect } = require("chai");
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Contract, ContractFactory } from "ethers";
 
 describe("Token contract", function () {
     it("Deployment should assign the total supply of tokens to the owner", async function () {
-        const [owner] = await ethers.getSigners();
-        const Token = await ethers.getContractFactory("Token");
-        const hardhatToken = await Token.deploy();
-        const ownerBalance = await hardhatToken.balanceOf(owner.address);
+        const allSigners: SignerWithAddress[] = await ethers.getSigners();
+        const owner: SignerWithAddress = allSigners[0]
+        const Token: ContractFactory = await ethers.getContractFactory("Token");
+        const hardhatToken: Contract = await Token.deploy();
+        const ownerBalance: Number = await hardhatToken.balanceOf(owner.address);
        
+        console.log(allSigners.map((signer: SignerWithAddress): string => signer.address))
+        
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
     });
 });
